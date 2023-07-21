b17f9be2aa54a225980e1b88389a0b5df2fcffb7
patel rohit <moghariyarohit@gmail.com>
2023-07-21 17:51:39 +0530

    ad some CSS file  ROHIT

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <center style={{color:"red",padding:"50px",fontSize:"50px"}}>Hello Kaushalam...How are You</center>
-    <h>Good Afternoon</h>
+    <h1>Good Afternoon</h1>
     </>
   );
 }
